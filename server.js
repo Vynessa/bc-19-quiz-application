2dc38f28a65430fa8a1c1a115f9cf077c894e6c6
vynessa <vanessa.ejikeme@gmail.com>
2016-12-08 16:08:16 +0100

    Modify user interface with a hompage and sweetalert popup message

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var app = express();
-var path = require('path');
+var path = require("path");
 var admin = require("firebase-admin");
-var firebase = require('firebase');
+var firebase = require("firebase");
 
  var config = {
   apiKey: "AIzaSyBrpB0CTavWdcN3NQcR07xM6UKz8WZtqWg",
@@ -17,22 +17,25 @@ firebase.initializeApp(config);
 var port = 3030;
 
 app.set('view engine', 'ejs')
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
+	res.render('index', { title: 'Quiz' });
+});
+
+app.get('/generaltech', function(req, res) {
 	firebase.database().ref('questions').once('value').then(function(questions){
-		res.render('index', { questions: questions.val() });
+		res.render('generaltech', { questions: questions.val() });
 	});
 });
 
-app.get('/leaderboard', function(req, res){
+// app.get('/leaderboard', function(req, res){
+// 	res.render('leaderboard', { title: 'Leaderboard' });
+// });
 
-	res.render('leaderboard', { title: 'Leaderboard' });
-});
-
-app.get('/quiz', function(req, res){
-	res.render('quiz', { title: 'Quiz' });
-});
+// app.get('/quiz', function(req, res){
+// 	res.render('quiz', { title: 'Quiz' });
+// });
 
 app.listen(port, function() {
 	console.log("Server running on port " + port);
